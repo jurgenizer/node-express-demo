c3f21bb8fa4977f80969ed3f1ab48b13b957d837
Jurgen <jurgen.geitner@gmail.com>
2024-09-16 19:07:30 +0200

    route parameters

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+// All courses
 app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
+
+// Single course
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+
+// Multiple route parametes
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
+// Query string parametes
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
+});
+
 
 //Environment Variable called PORT
 const port = process.env.PORT || 3000;
