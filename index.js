20c6ada87b2491303da90197fecd972e6c215769
Jurgen <jurgen.geitner@gmail.com>
2024-09-16 13:40:52 +0200

    environment variables

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!');
 });
 
 app.get('/api/courses', (req, res) => {
     res.send([1,2,3]);
 });
 
+//Environment Variable called PORT
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('Listening on Port 3000...'));
+app.listen(port, () => console.log(`Listening on Port ${port}`));
