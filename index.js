1efdef69497b59dc37290d3c2c070ffcf1d09d70
Jurgen <jurgen.geitner@gmail.com>
2024-09-17 17:06:10 +0200

    delete request

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/api/courses', (req, res) => {
 // Single course
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the requested ID was not found');
+    if (!course) return res.status(404).send('The course with the requested ID was not found');
     res.send(course);
 });
 
@@ -41,13 +41,9 @@ app.get('/api/posts/:year/:month', (req, res) => {
 /* POST */
 
 app.post('/api/courses', (req, res) => {
-     //validateCourse() with object destructuring to get error property
-     const { error } = validateCourse(req.body);
-     if (error) {
-         // 400 bad request
-         res.status(400).send(error.details[0].message);
-         return;
-     }
+    //validateCourse() with object destructuring to get error property
+    const { error } = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -59,11 +55,11 @@ app.post('/api/courses', (req, res) => {
 /* PUT */
 
 app.put('/api/courses/:id', (req, res) => {
-
     // Lookup the course
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the requested ID was not found');
-
+    if (!course) return res.status(404).send('The course with the requested ID was not found');
+     
+    
     //validateCourse() with tradtional JS 
     //const result = validateCourse(req.body);
     //validateCourse() with object destructuring to get error property
@@ -74,10 +70,10 @@ app.put('/api/courses/:id', (req, res) => {
         return;
     }
     // Update the course
-   course.name = req.body.name;
+    course.name = req.body.name;
 
     //Return the updated course
-   res.send(course);
+    res.send(course);
 
 });
 function validateCourse(course) {
@@ -87,7 +83,22 @@ function validateCourse(course) {
     return schema.validate(course);
 }
 
+/* DELETE*/
+app.delete('api/courses/:id', (req, res) => {
+    // Lookup the course
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    console.log('The course to be deleted is ', course);
+    // Not existing, return 404
+    if (!course) return res.status(404).send('The course with the requested ID was not found');
 
+    // Delete 
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    // Return the same course
+    res.send(course);
+
+});
 
 
 //Environment Variable called PORT
