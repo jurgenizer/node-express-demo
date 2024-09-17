8b3cf238eb73ce2416c725b31b217297e17b950a
Jurgen <jurgen.geitner@gmail.com>
2024-09-17 16:30:43 +0200

    put requests

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const courses = [
-    {id: 1, name: 'courseOne'},
-    {id: 2, name: 'courseTwo'},
-    {id: 3, name: 'courseThree'},
+    { id: 1, name: 'courseOne' },
+    { id: 2, name: 'courseTwo' },
+    { id: 3, name: 'courseThree' },
 ]
 
 /* GET */
@@ -21,9 +21,9 @@ app.get('/api/courses', (req, res) => {
 
 // Single course
 app.get('/api/courses/:id', (req, res) => {
-   const course = courses.find(c => c.id === parseInt(req.params.id));
-   if (!course) res.status(404).send('The course with the requested ID was not found');
-   res.send(course);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the requested ID was not found');
+    res.send(course);
 });
 
 
@@ -41,17 +41,13 @@ app.get('/api/posts/:year/:month', (req, res) => {
 /* POST */
 
 app.post('/api/courses', (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    }) 
-   const result = schema.validate(req.body);
-   // console.log(result);
-
-    if (result.error){
-        // 400 bad request
-        res.status(400).send(result.error.details[0].message);
-        return;
-    } 
+     //validateCourse() with object destructuring to get error property
+     const { error } = validateCourse(req.body);
+     if (error) {
+         // 400 bad request
+         res.status(400).send(error.details[0].message);
+         return;
+     }
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -59,6 +55,39 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 })
+
+/* PUT */
+
+app.put('/api/courses/:id', (req, res) => {
+
+    // Lookup the course
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the requested ID was not found');
+
+    //validateCourse() with tradtional JS 
+    //const result = validateCourse(req.body);
+    //validateCourse() with object destructuring to get error property
+    const { error } = validateCourse(req.body);
+    if (error) {
+        // 400 bad request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    // Update the course
+   course.name = req.body.name;
+
+    //Return the updated course
+   res.send(course);
+
+});
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    return schema.validate(course);
+}
+
+
 
 
 //Environment Variable called PORT
