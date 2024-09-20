5210a4b46570c8237295f310f5db10b580e38f91
Jurgen <jurgen.geitner@gmail.com>
2024-09-20 16:17:14 +0200

    Simple debug

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('app:startup');
 const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -19,15 +20,12 @@ app.use(helmet());
 // Configuration
 console.log('Application Name: ' +  config.get('name'));
 console.log('Mail Server: ' +  config.get('mail.host'));
-console.log('Mail Password: ' +  config.get('mail.password'));
-
-
-
-
+// console.log('Mail Password: ' +  config.get('mail.password'));
 
 if (app.get('env') === 'development'){
     app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+    //console.log('Morgan enabled...');
+    debug('Morgan enabled...');
 }
 
 // Custom third-part middleware by me :)
